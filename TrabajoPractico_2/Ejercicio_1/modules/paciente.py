e69d870787d674db50255bdc4c3a27ee61c81188
NahirJacobo <nahirjacobo47@gmail.com>
2025-05-23 13:30:34 -0300

    god explicacion

--- TrabajoPractico_2/Ejercicio_1/modules/paciente.py
+++ TrabajoPractico_2/Ejercicio_1/modules/paciente.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from random import randint, choices
+import time
 
 nombres = ['Leandro', 'Mariela', 'Gastón', 'Andrea', 'Antonio', 'Estela', 'Jorge', 'Agustina']
 apellidos = ['Perez', 'Colman', 'Rodriguez', 'Juarez', 'García', 'Belgrano', 'Mendez', 'Lopez']
@@ -11,12 +12,13 @@ descripciones_de_riesgo = ['crítico', 'moderado', 'bajo']
 probabilidades = [0.1, 0.3, 0.6] 
 
 class Paciente:
-    def __init__(self):
+    def __init__(self, tiempo_de_llegada):
         n = len(nombres)
         self.__nombre = nombres[randint(0, n-1)]
         self.__apellido = apellidos[randint(0, n-1)]
         self.__riesgo = choices(niveles_de_riesgo, probabilidades)[0]
         self.__descripcion = descripciones_de_riesgo[self.__riesgo-1]
+        self.__tiempo = tiempo_de_llegada
 
     def get_nombre(self):
         return self.__nombre
@@ -33,9 +35,21 @@ class Paciente:
     def __str__(self):
         cad = self.__nombre + ' '
         cad += self.__apellido + '\t -> '
-        cad += str(self.__riesgo) + '-' + self.__descripcion
+        cad += str(self.__riesgo) + '-' + self.__descripcion + '-' + str(self.__tiempo)
         return cad
+    
+    def __lt__ (self, otro):
+        pass
+
         
         
-        
+if __name__ == "__main__":
+    p1 = Paciente(time.time())
+    print ( p1)
+    time.sleep(5)
+    p2 = Paciente(time.time())
+    print(p2)        
+
+    if p1 < p2:
+        print( "p1 es menor que p2")
         
