de4a9ed570f729255bbe13bb1e471791181187d8
NahirJacobo <nahirjacobo47@gmail.com>
2025-05-27 09:46:38 -0300

    1er Ejercicio adentro

--- TrabajoPractico_2/Ejercicio_1/main.py
+++ TrabajoPractico_2/Ejercicio_1/main.py
@@ -9,41 +9,36 @@ import modules.paciente as pac
 import random
 
 n = 20  # cantidad de ciclos de simulación
-
 cola_de_espera = list()
 
 # Ciclo que gestiona la simulación
 for i in range(n):
-     #Fecha y hora de entrada de un paciente
     ahora = datetime.datetime.now()
     fecha_y_hora = ahora.strftime('%d/%m/%Y %H:%M:%S')
-    print('-*-'*15)
+    print('-*-' * 15)
     print('\n', fecha_y_hora, '\n')
 
-    # Se crea un paciente un paciente por segundo
-    # La criticidad del paciente es aleatoria
-    paciente = pac.Paciente()
+    # Se crea un paciente con hora de llegada actual
+    paciente = pac.Paciente(ahora)
     cola_de_espera.append(paciente)
 
     # Atención de paciente en este ciclo: en el 50% de los casos
-    if random.random() < 0.5:
-        # se atiende paciente que se encuentra al frente de la cola
+    if random.random() < 0.5 and cola_de_espera:
         paciente_atendido = cola_de_espera.pop(0)
-        print('*'*40)
+        print('*' * 40)
         print('Se atiende el paciente:', paciente_atendido)
-        print('*'*40)
-    else:
-        # se continúa atendiendo paciente de ciclo anterior
-        pass
-    
+        print('*' * 40)
+
     print()
 
-    # Se muestran los pacientes restantes en la cola de espera
+    # Ordenar la cola antes de mostrarla: primero por riesgo, luego por hora de llegada
+    cola_de_espera = sorted(cola_de_espera, key=lambda p: (p.get_riesgo(), p.get_tiempo()))
+
     print('Pacientes que faltan atenderse:', len(cola_de_espera))
     for paciente in cola_de_espera:
         print('\t', paciente)
-    
+
     print()
-    print('-*-'*15)
-    
+    print('-*-' * 15)
+
     time.sleep(1)
--- TrabajoPractico_2/Ejercicio_1/modules/paciente.py
+++ TrabajoPractico_2/Ejercicio_1/modules/paciente.py
@@ -1,57 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from random import randint, choices
-import time
 
 nombres = ['Leandro', 'Mariela', 'Gastón', 'Andrea', 'Antonio', 'Estela', 'Jorge', 'Agustina']
 apellidos = ['Perez', 'Colman', 'Rodriguez', 'Juarez', 'García', 'Belgrano', 'Mendez', 'Lopez']
 
 niveles_de_riesgo = [1, 2, 3]
 descripciones_de_riesgo = ['crítico', 'moderado', 'bajo']
-# probabilidades de aparición de cada tipo de paciente
 probabilidades = [0.1, 0.3, 0.6] 
 
 class Paciente:
-    def __init__(self): #tiempo_de_llegada):
+    def __init__(self, tiempo_de_llegada):  # <-- Acepta la hora
         n = len(nombres)
-        self.__nombre = nombres[randint(0, n-1)]
-        self.__apellido = apellidos[randint(0, n-1)]
+        self.__nombre = nombres[randint(0, n - 1)]
+        self.__apellido = apellidos[randint(0, n - 1)]
         self.__riesgo = choices(niveles_de_riesgo, probabilidades)[0]
-        self.__descripcion = descripciones_de_riesgo[self.__riesgo-1]
-        #self.__tiempo = tiempo_de_llegada
+        self.__descripcion = descripciones_de_riesgo[self.__riesgo - 1]
+        self.__tiempo = tiempo_de_llegada  # <-- Guarda la hora como datetime
 
     def get_nombre(self):
         return self.__nombre
-    
+
     def get_apellido(self):
         return self.__apellido
-    
+
     def get_riesgo(self):
         return self.__riesgo
-    
+
     def get_descripcion_riesgo(self):
         return self.__descripcion
-    
-    def __str__(self):
-        cad = self.__nombre + ' '
-        cad += self.__apellido + '\t -> '
-        cad += str(self.__riesgo) + '-' + self.__descripcion # + '-' + str(self.__tiempo)
-        return cad
-    
-        
-# inicio = time.perf_counter()
-# bubble_sort(lista)
-# fin = time.perf_counter()
-# tiempo_ejecucionBubble = fin - inicio
-# print(f"Tiempo de ejecución Bubblesort: {tiempo_ejecucionBubble:.4f} segundos")
-        
-# if __name__ == "__main__":
-#     p1 = Paciente(time.time())
-#     print ( p1)
-#     time.sleep(5)
-#     p2 = Paciente(time.time())
-#     print(p2)        
 
-#     if p1 < p2:
-#         print( "p1 es menor que p2")
-        
+    def get_tiempo(self):
+        return self.__tiempo
+
+    def __str__(self):
+        hora_formateada = self.__tiempo.strftime('%H:%M:%S')
+        cad = f"{self.__nombre} {self.__apellido:<10}\t -> {self.__riesgo}-{self.__descripcion} ({hora_formateada})"
+        return cad
