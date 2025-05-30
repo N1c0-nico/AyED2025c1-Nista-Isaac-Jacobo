b1da4d93922f6b592b4aa6787a34779d37c695d7
priscilaaisaac <priscilaisaac22@gmail.com+priscilaaisaac@users.noreply.github.com>
2025-05-30 14:06:09 -0300

    jodeeer

--- TrabajoPractico_2/Ejercicio_1/main.py
+++ TrabajoPractico_2/Ejercicio_1/main.py
@@ -1,89 +1,73 @@
-# # -*- coding: utf-8 -*-
-# """
-# Sala de emergencias
-# """
-
 import time
 import datetime
+import modules.paciente as pac
 import random
-from modules.paciente import Paciente 
-from modules.ColaPrioridad import ColaDePrioridad
+from modules.Monticulo import MonticuloBinario
 
-cola_prioritaria = ColaDePrioridad()
-n = 20  # ciclos de simulación
+# Simulación con el montículo personalizado
+n = 20  # cantidad de ciclos de simulación
+cola_de_espera = MonticuloBinario()  # Usamos el montículo para gestionar los pacientes
 
+# Ciclo que gestiona la simulación
 for i in range(n):
     ahora = datetime.datetime.now()
+    fecha_y_hora = ahora.strftime('%d/%m/%Y %H:%M:%S')
     print('-*-'*15)
-    print('\n', ahora.strftime('%d/%m/%Y %H:%M:%S'), '\n')
+    print('\n', fecha_y_hora, '\n')
 
-    # Se crea paciente con hora actual
-    paciente = Paciente(ahora)
-    cola_prioritaria.encolar(paciente)
+    paciente = pac.Paciente()
+    cola_de_espera.insertar(paciente)
 
-    # 50% de probabilidad de atender a alguien
-    if random.random() < 0.5:
-        paciente_atendido = cola_prioritaria.desencolar()
-        if paciente_atendido:
-            print('*'*40)
-            print('Se atiende el paciente:', paciente_atendido)
-            print('*'*40)
-        else:
-            print("No hay pacientes para atender.")
+    if random.random() < 0.5 and not cola_de_espera.esta_vacio():
+        paciente_atendido = cola_de_espera.extraer()
+        print('*'*40)
+        print('Se atiende el paciente:', paciente_atendido)
+        print('*'*40)
     else:
-        pass  # se sigue atendiendo paciente anterior
+        pass
 
     print()
-    print('Pacientes que faltan atenderse:', cola_prioritaria.monticulo.tamanoActual)
-    for i in range(1, cola_prioritaria.monticulo.tamanoActual + 1):
-        print('\t', cola_prioritaria.monticulo.listaMonticulo[i][2])
+    print('Pacientes que faltan atenderse:', cola_de_espera.cantidad())
+    cola_de_espera.mostrar_pacientes()
 
     print()
     print('-*-'*15)
+    
     time.sleep(1)
-
 
 # import time
 # import datetime
-# import modules.paciente as pac
+# from modules.paciente import Paciente
 # import random
+# from modules.ColaPrioridad import ColaDePrioridad  # tu nueva clase
 
 # n = 20  # cantidad de ciclos de simulación
+# cola_prioritaria = ColaDePrioridad()
 
-# cola_de_espera = list()
-
-# # Ciclo que gestiona la simulación
 # for i in range(n):
-#     # Fecha y hora de entrada de un paciente
 #     ahora = datetime.datetime.now()
-#     fecha_y_hora = ahora.strftime('%d/%m/%Y %H:%M:%S')
-#     print('-*-'*15)
-#     print('\n', fecha_y_hora, '\n')
+#     print('-*-' * 15)
+#     print('\n', ahora.strftime('%d/%m/%Y %H:%M:%S'), '\n')
 
-#     # Se crea un paciente un paciente por segundo
-#     # La criticidad del paciente es aleatoria
-#     paciente = pac.Paciente()
-#     cola_de_espera.append(paciente)
+#     # Crear nuevo paciente con hora actual
+#     paciente = Paciente(ahora)
 
-#     # Atención de paciente en este ciclo: en el 50% de los casos
-#     if random.random() < 0.5:
-#         # se atiende paciente que se encuentra al frente de la cola
-#         paciente_atendido = cola_de_espera.pop(0)
-#         print('*'*40)
+#     if random.random() < 0.5 and not cola_prioritaria.esta_vacia():
+#         # Se atiende el paciente más prioritario
+#         paciente_atendido = cola_prioritaria.desencolar()
+#         print('*' * 40)
 #         print('Se atiende el paciente:', paciente_atendido)
-#         print('*'*40)
+#         print('*' * 40)
 #     else:
-#         # se continúa atendiendo paciente de ciclo anterior
-#         pass
-    
-#     print()
+#         # El nuevo paciente entra a la cola de espera
+#         cola_prioritaria.encolar(paciente)
 
-#     # Se muestran los pacientes restantes en la cola de espera
-#     print('Pacientes que faltan atenderse:', len(cola_de_espera))
-#     for paciente in cola_de_espera:
-#         print('\t', paciente)
-    
 #     print()
-#     print('-*-'*15)
-    
+#     print('Pacientes que faltan atenderse:', len(cola_prioritaria.pacientes_en_espera()))
+#     for p in cola_prioritaria.pacientes_en_espera():
+#         print('\t', p)
+
+#     print()
+#     print('-*-' * 15)
+
 #     time.sleep(1)
--- TrabajoPractico_2/Ejercicio_1/modules/ColaPrioridad.py
+++ TrabajoPractico_2/Ejercicio_1/modules/ColaPrioridad.py
@@ -1,16 +1,20 @@
 from modules.Monticulo import MonticuloBinario
-
 class ColaDePrioridad:
     def __init__(self):
         self.monticulo = MonticuloBinario()
 
     def encolar(self, paciente):
-        riesgo = paciente.get_riesgo()
-        tiempo = paciente.get_tiempo().timestamp()
-        # Insertar una tupla (prioridad principal, secundaria, objeto)
-        self.monticulo.insertar((riesgo, tiempo, paciente))
+        # Menor riesgo tiene más prioridad: (1, hora)
+        clave = (paciente.get_riesgo(), paciente.get_tiempo(), paciente)
+        self.monticulo.insertar(clave)
 
     def desencolar(self):
         if self.monticulo.tamanoActual == 0:
             return None
-        return self.monticulo.eliminarMin()[2]
+        return self.monticulo.eliminarMin()[2]  # Solo devolver el paciente
+
+    def esta_vacia(self):
+        return self.monticulo.tamanoActual == 0
+
+    def pacientes_en_espera(self):
+        return [self.monticulo.listaMonticulo[i][2] for i in range(1, self.monticulo.tamanoActual + 1)]
